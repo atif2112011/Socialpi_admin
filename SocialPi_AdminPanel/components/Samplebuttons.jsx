40de716f6b743bbd3b1ce1fd94fad91443840426
Blaze1012 <atif251171@gmail.com>
2023-10-20 18:45:18 +0530

    Added Homepage

---
+++ SocialPi_AdminPanel/components/Samplebuttons.jsx
@@ -0,0 +1,83 @@
+function Samplebuttons(){
+    return(
+        <>
+        <div class="flex-col">
+          <h1>Small</h1>
+        <div class="flex">
+          <div class="flex-col">
+          <button class="btn-primary-small-text">Button</button>
+          
+          
+          <button class="btn-primary-small-icon"><i class="fa fa-check"></i></button>
+          
+          
+          <button class="btn-primary-small-button-icon">Button         <i class="fa fa-check"></i></button>
+          
+          <button class="btn-primary-small-button-icon"><i class="fa fa-check"></i> Button</button>
+        </div>
+        <div class="flex-col">
+          <button class="btn-secondary-small-text">Button</button>
+          
+          
+          <button class="btn-secondary-small-icon"><i class="fa fa-check"></i></button>
+          
+          
+          <button class="btn-secondary-small-button-icon">Button <i class="fa fa-check"></i></button>
+          
+          <button class="btn-secondary-small-button-icon"><i class="fa fa-check"></i> Button</button>
+        </div>
+        <div class="flex-col">
+          <button class="btn-tertiary-small-text">Button</button>
+          
+          
+          <button class="btn-tertiary-small-icon"><i class="fa fa-check"></i></button>
+          
+          
+          <button class="btn-tertiary-small-button-icon">Button <i class="fa fa-check"></i></button>
+          
+          <button class="btn-tertiary-small-button-icon"><i class="fa fa-check"></i> Button</button>
+        </div>
+          </div>
+          <h1>Medium</h1>
+          <div class="flex">
+          <div class="flex-col">
+          <button class="btn-primary-medium-text">Button</button>
+          
+          
+          <button class="btn-primary-medium-icon"><i class="fa fa-check"></i></button>
+          
+          
+          <button class="btn-primary-medium-button-icon">Button         <i class="fa fa-check"></i></button>
+          
+          <button class="btn-primary-medium-button-icon"><i class="fa fa-check"></i> Button</button>
+        </div>
+        <div class="flex-col">
+          <button class="btn-secondary-medium-text">Button</button>
+          
+          
+          <button class="btn-secondary-medium-icon"><i class="fa fa-check"></i></button>
+          
+          
+          <button class="btn-secondary-medium-button-icon">Button <i class="fa fa-check"></i></button>
+          
+          <button class="btn-secondary-medium-button-icon"><i class="fa fa-check"></i> Button</button>
+        </div>
+        <div class="flex-col">
+          <button class="btn-tertiary-medium-text">Button</button>
+          
+          
+          <button class="btn-tertiary-medium-icon"><i class="fa fa-check"></i></button>
+          
+          
+          <button class="btn-tertiary-medium-button-icon">Button <i class="fa fa-check"></i></button>
+          
+          <button class="btn-tertiary-medium-button-icon"><i class="fa fa-check"></i> Button</button>
+        </div>
+          </div>
+        </div>
+          
+        </>
+    )
+}
+
+export default Samplebuttons
---
+++ SocialPi_AdminPanel/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Samplebuttons from '../components/Samplebuttons'
+function App() {
+
+
+  return (
+   <Samplebuttons/>
+  // <div class='flex'>
+  //   <div class='flex-col menu'>
+
+  //   </div>
+  //   <div class='flex-col dashboard'>
+  //     <h1>Admin Dashboard</h1>
+  //   </div>
+  // </div>
+  )
+}
+
+export default App
